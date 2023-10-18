b745aeced7bdc7cb625c2e8ba95eafc8914cd57d
REZOAN93 <rezoanshawonweb@gmail.com>
2023-10-18 17:58:55 +0600

    updated the mongodb setting

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-require('dotenv').config()
+require("dotenv").config();
 
-const uri =
-  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.SECRET_KEY}@cluster0.lh0lzsv.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.SECRET_KEY}@cluster0.lh0lzsv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,16 +22,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+
+    const database = client.db("BrandShop");
+    const BrandNames = database.collection("BrandNames");
+
+    app.get("/brands", async (req, res) => {
+      const cursor = BrandNames.find();
+      const brandsDetails = await cursor.toArray();
+      res.send(brandsDetails);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
